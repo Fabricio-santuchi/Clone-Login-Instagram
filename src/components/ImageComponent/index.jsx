be318638fa8b8e485c4f444c7222c8ae9fe61d4f
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2024-10-28 21:10:00 -0300

    sv

--- src/components/ImageComponent/index.jsx
+++ src/components/ImageComponent/index.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 const ImageComponent = ({ src, alt }) => {
-  return <img className="h-14 max-w-full" src={src} alt={alt} />;
+  return <img className="h-14 max-w-full cursor-pointer" src={src} alt={alt} />;
 };
 
 export default ImageComponent;
