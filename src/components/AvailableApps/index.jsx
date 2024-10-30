255f40fdc4606c208cc600baabb93ec2fdc152c8
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2024-10-30 20:15:28 -0300

    sv

--- src/components/AvailableApps/index.jsx
+++ src/components/AvailableApps/index.jsx
@@ -6,7 +6,7 @@ const AvailableApps = () => {
   return (
     <section className="text-center mb-10">
       <p className="mb-4 text-lg">Obtenha o aplicativo.</p>
-      <div className="flex gap-2 flex-wrap justify-center items-center">
+      <div className="flex gap-2 justify-center items-center">
         <ImageComponent src={Gplay} alt="Baixar na Google Play" />
         <ImageComponent src={Msoft} alt="Baixar na microsoft Store" />
       </div>
